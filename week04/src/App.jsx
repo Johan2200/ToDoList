50f1116967bcba45283530286b5931279e1a5f97
jokt <jokt@itu.dk>
2026-09-17 12:17:27 +0200

    inital work in setting up the parse backend

--- week04/src/App.jsx
+++ week04/src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
 import ToDoList from "./ToDoList.jsx";
+import Parse from "parse";
 
 function App() {
+  Parse.initialize(
+    "lJ5r2MjkEiKZjS8mUb38Guz3UKDrmQ0Ugs0XZac9",
+    "N5UHESaVa5tVH7gQUxYITr2y87yLWB03WLyp1Xg7",
+  );
+
+  Parse.serverURL = "https://parseapi.back4app.com";
+
   const annasToDoList = [
     { id: "anna-1", text: "Call the landlord", done: false },
     { id: "anna-2", text: "Book the dentist", done: false },
