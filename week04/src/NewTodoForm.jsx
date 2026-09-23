f488cec18e48ef645844dd1f90306ec112663525
jokt <jokt@itu.dk>
2026-09-23 17:20:36 +0200

    add explanatory comments

--- week04/src/NewTodoForm.jsx
+++ week04/src/NewTodoForm.jsx
@@ -5,13 +5,14 @@ export default function NewTodoForm({ onAdd }) {
   const [text, setText] = useState("");
 
   function handleSubmit(event) {
-    event.preventDefault();
+    event.preventDefault(); // this makes it possible not to reload the entire page on submit
     onAdd(text);
     setText("");
   }
 
   return (
     <form onSubmit={handleSubmit}>
+      {/* onSubmit fires both on button click and Enter key (both phone and keyboard)*/}
       <input
         value={text}
         onChange={(e) => setText(e.target.value)}
