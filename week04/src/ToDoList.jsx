671783d52f682a424178efde5ffdcb3d191558e7
jokt <jokt@itu.dk>
2026-09-24 14:41:46 +0200

    add service layer for the ToDoList.jsx file to achieve better seperation of concerns

--- week04/src/ToDoList.jsx
+++ week04/src/ToDoList.jsx
@@ -1,98 +1,39 @@
 import { useState, useEffect } from "react";
+import {
+  fetchTodos,
+  createTodo,
+  setTodoDone,
+  deleteTodo,
+} from "./services/todoServices";
+import { use } from "react";
 import NewTodoForm from "./NewTodoForm";
 import ToDoItem from "./ToDoItem";
-import Parse from "parse";
-
-const TodoItem = Parse.Object.extend("TodoItem");
 
 export default function ToDoList({ firstName }) {
-  let h1style = { color: "deeppink", backgroundColor: "white" };
-
-  let [todos, setTodos] = useState([]);
-
-  async function loadTodos() {
-    // we are creating a query object for objects of type TodoItem
-    const query = new Parse.Query(TodoItem);
-
-    query.equalTo("done", false); //only show those not done
-    query.ascending("createdAt");
-
-    // await
-    const result = await query.find();
-
-    let todosInDB = [];
-
-    for (const item of results) {
-      todosInDB.push({
-        id: item.id,
-        text: item.get("text"),
-        done: item.get("done"),
-      });
-    }
-
-    setTodos(todosInDB);
-  }
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    loadTodos();
+    async function load() {
+      setTodos(await fetchTodos());
+    }
+    load();
   }, []);
 
-  // This is the function that adds a new task (newTask is a String)
-  function handleAdd(newTaskText) {
-    const item = new TodoItem(); // this creates a row in the table
-    item.set("text", newTaskText);
-    item.set("done", false);
-    item.save().then(onSuccessfulSave).catch(onError);
-
-    function onSuccessfulSave(savedItem) {
-      let newTodos = [
-        ...todos,
-        { id: savedItem.id, text: newTaskText, done: false },
-      ];
-      setTodos(newTodos);
-    }
-
-    function onError(error) {
-      alert(error.message);
-    }
+  async function handleAdd(newTask) {
+    const created = await createTodo(newTask);
+    setTodos([...todos, created]);
   }
 
-  function handleDelete(idToDelete) {
-    // as we only have the id, we must build a 'stand in' object pointing at that row
-    const item = ToDoItem.createWithoutData(idToDelete);
-
-    // destroy() deletes the row on the server; only then drop it from the screen
-    item
-      .destroy()
-      .then(() => {
-        let newTodos = todos.filter((each) => each.id !== idToDelete);
-        setTodos(newTodos);
-      })
-      .catch((error) => alert(error.message));
+  async function handleDelete(idToDelete) {
+    await deleteTodo(idToDelete);
+    setTodos(todos.filter((each) => each.id !== idToDelete));
   }
 
-  // check mark has been toggled
-  function handleToggle(id) {
-    const todo = todos.find((each) => each.id === id);
-
-    const item = TodoItem.createWithoutData(id);
-    item.set("done", !todo.done);
-
-    item
-      .save()
-      .then(() => {
-        let newTodos = todos.map((t) =>
-          t.id === id ? { ...t, done: !t.done } : t,
-        );
-
-        setTodos(newTodos);
-      })
-      .catch(() => console.log("something went wrong"));
+  async function handleToggle(id) {
+    const todo = todos.find((t) => t.id === id);
+    await setTodoDone(id, !todo.done);
+    setTodos(todos.map((t) => (t.id === id ? { ...t, done: !t.done } : t)));
   }
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
 
   return (
     <>
